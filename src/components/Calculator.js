e3e899a96de55f46abd8520fe531733b9ce40532
NGOCNHI1803 <tranthingocnhi1803@gmail.com>
2024-08-14 11:51:55 +0700

    start

---
+++ src/components/Calculator.js
@@ -0,0 +1,34 @@
+import './cal.css'
+function Mycal() {
+    return (
+        <div className="cal-grid">
+            <div className="output">
+                <div className="data"></div>
+                <div className="result-cal"></div>
+            </div>
+            <button>CE</button>
+            <button>C</button>
+            <button>%</button>
+            <button>/</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>*</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>-</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+            <button>0</button>
+            <button>,</button>
+            <button className="span-two">=</button>
+        </div>
+      
+    );
+  }
+  
+  export default Mycal;
+  
